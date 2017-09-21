20b62969b2b604bcc9b412cf17b909811e6ffe07
shane <g00332592@gmit.ie>
2017-09-21 19:54:19 +0100

    factorial start

---
+++ progs/factorial/factorial.go
@@ -0,0 +1,16 @@
+//factorial
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Factorial Digit Sum")
+	fmt.Println("===================\n")
+
+	for i := 0; i <= 100; i++ {
+
+	}//for
+
+}//main
