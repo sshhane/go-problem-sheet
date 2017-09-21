52c5f38a040354349d87964bfd0c7a43e74ab3cb
shane <g00332592@gmit.ie>
2017-09-21 19:28:49 +0100

    Correct output

--- progs/fizzbuzz/fizzbuzz.go
+++ progs/fizzbuzz/fizzbuzz.go
@@ -14,27 +14,13 @@ func main() {
 
 		if( i%3 == 0 && i%5 == 0 ) {
 		fmt.Println("FizzBuzz");
+		} else if i%3 == 0 {
+		fmt.Println("Fizz");
 		} else if i%5 == 0 {
 		fmt.Println("Buzz");
 		} else {
 		fmt.Printf("%d\n", i);
 		}
-
-		// if (i%3 == 0 && i%5 == 0) {
-		// 	fmt.Println("fizzbuzz")
-		// }
-
-		// else if i%3 == 0 {
-		// 	fmt.Println("Fizz")
-		// }
-
-		// else if i%5 == 0 {
-		// 	fmt.Println("Buzz")
-		// }
-
-		// else {
-		// 	fmt.Println(i)
-		// }
 
 	}//for
 
