a14d5be275ac5eef56fbc5bf677982d8c563efc3
shane <g00332592@gmit.ie>
2017-10-04 15:52:05 +0100

    large/small

---
+++ progs/largesmall/largesmall.go
@@ -0,0 +1,23 @@
+// largest & smallest in list
+package main
+import "fmt"
+
+func main() {
+	fmt.Println("Largest / Smallest")
+
+	size := 10
+	// init array
+	var numbers [10]int
+
+	// ask for nums
+	fmt.Println("Enter 10 numbers:")
+
+	fmt.Println(size)
+	fmt.Println(numbers[0])
+	// fmt.Scanf("%d ", &numbers)
+
+	// pop array
+	// for i:=0; i<=size
+
+	// print array
+}
