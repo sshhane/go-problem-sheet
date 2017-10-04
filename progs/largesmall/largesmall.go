130aa75be3f5ffc14588687ed46f8b60824c5438
shane <g00332592@gmit.ie>
2017-10-04 19:00:21 +0100

    minMax

--- progs/largesmall/largesmall.go
+++ progs/largesmall/largesmall.go
@@ -2,6 +2,30 @@
 package main
 import "fmt"
 
+// func minMax(numbers [10]int) int, int {
+
+// 	// init min / max
+// 	var min int = numbers[0]
+// 	var max int = numbers[0]
+
+// 	// iterate overf array
+
+
+// 	return min, max
+// }
+
+//function to produce min and max values
+func minMax(array [10]int) (int, int) {
+
+	//initialise min / max
+	var min int = array[0]
+	var max int = array[0]
+
+	//iterate over array
+    // for...
+    return min, max
+}
+
 func main() {
 	fmt.Println("Largest / Smallest")
 
@@ -20,5 +44,7 @@ func main() {
 
 	}
 
-	// print array
+	// print min max
+	fmt.Printf("Min and max are: ")
+	fmt.Println(minMax(numbers))
 }
