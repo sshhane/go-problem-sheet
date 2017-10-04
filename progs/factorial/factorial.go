2bbbf84e2a5d5b45b18e0ef8dc2d212d2bcece65
shane <g00332592@gmit.ie>
2017-10-04 13:59:07 +0100

    sumOfDigits func

--- progs/factorial/factorial.go
+++ progs/factorial/factorial.go
@@ -10,8 +10,11 @@ var n int
  
 /*     function declaration        */
 func factorial(n int) uint64 {   
+
+	// check if negative
     if(n < 0){
-        fmt.Print("Factorial of negative number doesn't exist.")    
+        fmt.Print("Please use a positive number:")   
+
     }else{        
         for i:=1; i<=n; i++ {
             factVal *= uint64(i)  // mismatched types int64 and int
@@ -26,12 +29,23 @@ func factorial(n int) uint64 {
 // 	sum = factVal + 1
 // 	return sum
 // }//sum
+
+func sumOfDigits(factVal uint64) uint64{
+
+    // String digits = new Integer(factVal).toString();
+    var sum uint64 = 0
+    sum = factVal + 1
+    // int sum = 0;
+    // for (char c: digits.toCharArray())
+    //     sum += c - '0';
+    return sum;
+}
  
 func main(){    
     fmt.Print("Enter a positive integer between 0 - 50 : ")
     fmt.Scan(&n)
     fmt.Print("Factorial is: ",factorial(n))
 	// fmt.Print("Factorial is: ",factVal)
-	// fmt.Print("Sum is: ",sum(factVal))
+	fmt.Print("Sum is: ",sumOfDigits(factVal))
 
 }//main
