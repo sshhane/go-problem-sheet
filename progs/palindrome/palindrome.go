7492ed323c88dea2bc0641b422816bf25794435b
shane <g00332592@gmit.ie>
2017-10-04 21:23:43 +0100

    input

---
+++ progs/palindrome/palindrome.go
@@ -0,0 +1,21 @@
+// palindrome test
+package main
+
+import (
+	"fmt"
+	// "strings"
+)
+
+func main() {
+	fmt.Println("Palindrome test")
+
+	// vars
+	var myStr string
+
+	// ask for string
+	fmt.Println("Enter string:")
+	fmt.Scanf("%s\n", &myStr)
+
+	
+	// 
+}
