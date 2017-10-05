41065d4ff3389a5b1425fb3f86bc841b40c698d1
shane <g00332592@gmit.ie>
2017-10-05 18:40:08 +0100

    pop arrays

--- progs/mergesort/mergesort.go
+++ progs/mergesort/mergesort.go
@@ -10,11 +10,18 @@ func main() {
 	// vars
 	count := 3
 	var listOne[3]int
-	// var listTwo[3]int
+	var listTwo[3]int
 
-	// poulate first array
+	// poulate arrays
+	fmt.Println("First array")
 	for i := 0; i < count; i++ {
 		fmt.Printf("Enter number %d:", i+1)
 		fmt.Scanf("%d ", &listOne[i])
+	}
+
+	fmt.Println("\nSecond array")
+	for i := 0; i < count; i++ {
+		fmt.Printf("Enter number %d:", i+1)
+		fmt.Scanf("%d ", &listTwo[i])
 	}
 }
