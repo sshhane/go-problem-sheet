0c7dd98ed128290605fe4da421ccb14e4b1d9d9a
shane <g00332592@gmit.ie>
2017-10-07 00:04:15 +0100

    reverse string

--- reverse/reverse.go
+++ reverse/reverse.go
@@ -1,10 +1,16 @@
 // reverse a string
+// https://coderwall.com/p/fw6miw/reverse-text-in-golang
 
 package main
 import "fmt"
 
 func Reverse(s string) string {
-
+	var result string
+	//end of s to start
+    for i := len(s)-1; i >= 0; i-- {
+        result += string(s[i])	// populate result with chars in element
+    }
+    return result 
 }
 
 //main function 
@@ -13,6 +19,8 @@ func main() {
 	var str string
 	//prompt
 	fmt.Println("Enter string: ")
-
+	fmt.Scanf("%s ", &str)
+	
 	//output result
+	fmt.Printf("\nReverse of %s: %s", str, Reverse(str))
 }
